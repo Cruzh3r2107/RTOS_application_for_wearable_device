a0e4a2b959c166d91b78bdf1cf67868450695acc
Cruzh3r2107 <vshwnth.sngh@gmail.com>
2025-03-18 16:50:05 -0700

    Update flash_storage.h

--- flash_storage.h
+++ flash_storage.h
@@ -5,12 +5,12 @@
 #include <drivers/flash.h>
 
 // Flash storage configuration
-//These values are defined using Zephyr RTOSs' device tree macros
-#define FLASH_BASE_ADDRESS DT_FLASH_AREA_STORAGE_OFFSET //starting address of flash memory
-#define FLASH_AREA_SIZE DT_FLASH_AREA_STORAGE_SIZE //size of the flash
+#define FLASH_BASE_ADDRESS ((size_t)DT_FLASH_AREA_STORAGE_OFFSET) // Start address of flash memory
+#define FLASH_AREA_SIZE ((size_t)DT_FLASH_AREA_STORAGE_SIZE) // Size of flash memory
 
 // Function declarations
 void flash_init(void);
 void flash_task(void);
+void flash_erase(void); // Function to erase flash memory
 
 #endif // FLASH_STORAGE_H
